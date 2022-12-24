99fae53e9299d50f716608252b36ef1d83e3194e
VashenkoNikita <>
2022-12-24 21:20:03 +0400

    Create project

---
+++ Multiversity/MultiversityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiversityApp.swift
+//  Multiversity
+//
+//  Created by NikitaV on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiversityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
